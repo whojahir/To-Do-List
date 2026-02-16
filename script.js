177917d61c5d2ab4404b8106410d7161389dad38
Jahir Hossain <jahirhossain5851843@gmail.com>
2026-02-16 16:51:44 +0530

    Updated code

--- script.js
+++ script.js
@@ -3,16 +3,37 @@ const input = document.querySelector("#todo-input");
 const addBtn = document.querySelector("#add-btn");
 const todoList = document.querySelector("#todo-list");
 
-addBtn.addEventListener("click", function() {
+addBtn.addEventListener("click", function () {
     const taskText = input.value;
-    
+
     if (taskText === "") {
         alert("Please enter a task");
         return;
     }
 
+    // Create List Item
     const li = document.createElement("li");
     li.textContent = taskText;
     todoList.appendChild(li);
+
+    // Create Delete Button
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+
+    li.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click", function () {
+        todoList.removeChild(li);
+    })
+
+    li.addEventListener("click", function (e) {
+
+        if (e.target.tagName === "BUTTON") {
+            return;
+        }
+
+        li.classList.toggle("completed");
+    });
+
     input.value = "";
-})
+});
